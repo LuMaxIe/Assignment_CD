e6fa5e6e61b5b790ebad1d4a67cd39da5d474bcc
LuMaxIe <max.lugthart@icloud.com>
2022-04-06 15:05:26 +0200

    failing test

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Import what we need from flask
 from flask import Flask
 
-app = Flask("__name__")
+app = Flask(__name__)
 
 @app.route('/')
 def index():
@@ -10,4 +10,6 @@ def index():
 @app.route('/cow')
 def cow():
     return 'MOoooOo!'
-    
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
